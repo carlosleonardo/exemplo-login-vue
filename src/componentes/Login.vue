d2c5522676b8d583e32424334ce18a1dd1232976
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2025-03-06 10:15:43 -0300

    Inclusão de pacote primevue e seus temas, mais tela de login

---
+++ src/componentes/Login.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import Card from "primevue/card";
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+import Password from "primevue/password";
+</script>
+
+<template>
+    <h1>Login</h1>
+    <form>
+        <div class="p-field">
+            <label for="email">E-mail</label>
+            <InputText id="email" v-model="email" type="email" />
+        </div>
+        <div class="p-field">
+            <label for="password">Password</label>
+            <Password id="password" v-model="password" />
+        </div>
+        <Button type="submit" label="Login" />
+    </form>
+</template>
+
+<style scoped>
+:host {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    place-content: center;
+    height: 100vh;
+}
+</style>
