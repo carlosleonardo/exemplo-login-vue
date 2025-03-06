c688c664d7785bf3f526015eba43abad01de390b
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2025-03-06 10:49:28 -0300

    Alteração para validar botão de login

--- src/componentes/Login.vue
+++ src/componentes/Login.vue
@@ -1,8 +1,20 @@
-<script lang="ts" setup>
+<script setup lang="ts">
 import Card from "primevue/card";
 import Button from "primevue/button";
 import InputText from "primevue/inputtext";
 import Password from "primevue/password";
+import { ref } from "vue";
+
+const email = ref<string>("");
+const password = ref<string>("");
+const invalido = ref<boolean>(true);
+
+function aoMudarEmail() {
+    invalido.value = email.value === "" || password.value === "";
+}
+function aoMudarSenha() {
+    invalido.value = email.value === "" || password.value === "";
+}
 </script>
 
 <template>
@@ -10,13 +22,19 @@ import Password from "primevue/password";
     <form>
         <div class="p-field">
             <label for="email">E-mail</label>
-            <InputText id="email" v-model="email" type="email" />
+            <InputText
+                id="email"
+                v-model="email"
+                type="email"
+                @change="aoMudarEmail"
+            />
         </div>
         <div class="p-field">
             <label for="password">Password</label>
-            <Password id="password" v-model="password" />
+            <Password id="password" v-model="password" @change="aoMudarSenha" />
         </div>
-        <Button type="submit" label="Login" />
+        <Button type="submit" label="Login" :disabled="invalido" />&nbsp;
+        <Button type="button" label="Cadastrar" />
     </form>
 </template>
 
